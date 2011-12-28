c98615d187575c7ec7a61971c995a6e6a558f654
Tim Glabisch <tim@ainfach.de>
2011-12-27 16:01:29 -0800

    plugin wird nun auch gefunden...

--- lib/Symfonydic/Plugin.php
+++ lib/Symfonydic/Plugin.php
@@ -3,51 +3,34 @@
 Zend_Controller_Action_HelperBroker::addHelper(new Symfonydic_Controller_Action_Helper_Symfonydic());
 Zend_Loader_Autoloader::getInstance()->setFallbackAutoloader(true);
 
-class Anydic_Plugin extends Pimcore_API_Plugin_Abstract implements Pimcore_API_Plugin_Interface {
-
+class Symfonydic_Plugin extends Pimcore_API_Plugin_Abstract implements Pimcore_API_Plugin_Interface {
 
     public static function install() {
 
         if (self::isInstalled()) {
-            $statusMessage = "AnyDic Plugin successfully installed.";
+            $statusMessage = "Symfonydic Plugin successfully installed.";
         } else {
-            $statusMessage = "AnyDic Plugin could not be installed";
+            $statusMessage = "Symfonydic Plugin could not be installed";
         }
         return $statusMessage;
-
     }
 
     public static function uninstall() {
 
         if (!self::isInstalled()) {
-            $statusMessage = "AnyDic Plugin successfully uninstalled.";
+            $statusMessage = "Symfonydic Plugin successfully uninstalled.";
         } else {
-            $statusMessage = "AnyDic Plugin could not be uninstalled";
+            $statusMessage = "AnySymfonydicDic Plugin could not be uninstalled";
         }
         return $statusMessage;
-
     }
 
     public static function isInstalled() {
        return true;
     }
 
-    public static function getTranslationFileDirectory() {
-        return PIMCORE_PLUGINS_PATH . "/AnyDic/texts";
-    }
-
-    /**
-     *
-     * @param string $language
-     * @return string path to the translation file relative to plugin direcory
-     */
     public static function getTranslationFile($language) {
-        if (is_file(PIMCORE_PLUGINS_PATH . "/AnyDic/texts/" . $language . ".csv")) {
-            return "/AnyDic/texts/" . $language . ".csv";
-        } else {
-            return "/AnyDic/texts/en.csv";
-        }
-
+            return "/Symfonydic/texts/en.csv";
     }
 
 }
